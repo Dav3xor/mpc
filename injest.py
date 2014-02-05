fdef36b6c25092794f1214d16faef73ae77e1cdf
David Case <Dav3xor@gmail.com>
2014-02-04 22:03:16 -0800

    made schema loader a function

--- injest.py
+++ injest.py
@@ -1,29 +1,37 @@
 import re
 
-person_schema = {}
-household_schema = {}
-types = { 'H':household_schema, 
-          'P':person_schema 
-        }
 
-read_values = False
+def loadschema(filename):
+  types = {}
 
-entry = re.compile(r"^\s+(?P<name>\w+)\s+(?P<start>\d+)-(?P<end>\d+)")
+  read_values = False
 
-with open('us1850a_records.txt') as records:
-  for line in records:
-    if 'record type' in line:
-      current = types[line.split('"')[1]]
-    elif 'data list' in line:
-      read_values = True
-    elif '.' in line:
-      read_values = False
-    elif read_values:
-      fields = entry.search(line)
-      try:
-        current[fields.group('name')] = [int(fields.group('start')),
-                                         int(fields.group('end')) ]
-      except:
-        print line + " . " + str(fields.groups())
-#print person_schema
-#print household_schema
+  entry = re.compile(r"^\s+(?P<name>\w+)\s+(?P<start>\d+)-(?P<end>\d+)")
+
+  with open(filename) as records:
+    for line in records:
+      if 'record type' in line:
+        rtype        = line.split('"')[1]
+        types[rtype] = {}
+        current     = types[rtype]
+      elif 'data list' in line:
+        read_values = True
+      elif '.' in line:
+        read_values = False
+      elif read_values:
+        fields = entry.search(line)
+        try:
+          current[fields.group('name')] = [int(fields.group('start')),
+                                           int(fields.group('end')) ]
+        except:
+          # if we can't parse, print something.
+          print line + " . " + str(fields.groups())
+      else:
+        #we are not interested in other lines
+        continue
+    # the file gets closed automatically when we
+    # leave the with block.
+  return types
+
+
+print loadschema('us1850a_records.txt')
